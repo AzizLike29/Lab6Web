ed52ca4f5000d783155262b9d99126d8a95c1ed0
AzizLike29 <abdulfirdaus590@gmail.com>
2023-04-14 22:33:23 +0700

    Praktikum 6 Lanjutan

---
+++ template/footer.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+  <footer>
+    <div>
+      <p>&copy; 2023, Teknik Informatika, <a href="https://wa.me/62895618748150" style="text-decoration: none;">Abdul Aziz Firdaus</a>, Universitas Pelita Bangsa</p>
+    </div>
+  </footer>
+</body>
+
+</html>
---
+++ template/sidenav.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Konsep Modularisasi</title>
+  <link href="css/main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <header>
+    <h1>Implementasi dengan modularisasi</h1>
+  </header>
+  <div class="sidenav">
+    <a href="module/motor.php">Home</a>
+    <a href="module/about.php">About</a>
+    <a href="module/artikel/form_input.php">Form</a>
+    <a href="module/contact.php">Contact</a>
+    <a href="module/data.php">Data Form</a>
+  </div>
+</body>
+
+</html>
